4f32941bd952311f9be909ef617867c78cd6a946
VideroDesigns <maxayprivate@gmail.com>
2021-01-13 09:25:30 +0530

    [🙃] Little Progress, Starting with the main part!

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from pytube import YouTube
 import re
 from typing import *
 import jsonify
+import datetime, calendar
 
 config_read = open('./config.json', 'r')
 config = json.load(config_read)
@@ -12,7 +13,88 @@ API_KEY = config["API_KEY"]
 client = build('youtube', 'v3', developerKey=API_KEY)
 app = Quart(__name__)
 
+def get_years(timeBetween, year, reverse):
+    years = 0
 
+    while True:
+        if reverse:
+            year -= 1
+        else:
+            year += 1
+
+        year_days = 366 if calendar.isleap(year) else 365 
+        year_seconds = year_days * 86400
+
+        if timeBetween < year_seconds:
+            break
+
+        years += 1
+        timeBetween -= year_seconds
+
+    return timeBetween, years, year
+def get_months(timeBetween, year, month, reverse):
+    months = 0
+
+    while True:
+        month_days = calendar.monthrange(year, month)[1]
+        month_seconds = month_days * 86400
+
+        if timeBetween < month_seconds:
+            break
+
+        months += 1
+        timeBetween -= month_seconds
+
+        if reverse:
+            if month > 1:
+                month -= 1
+            else:
+                month = 12
+                year -= 1
+        else:
+            if month < 12:
+                month += 1
+            else:
+                month = 1
+                year += 1
+
+    return timeBetween, months
+
+def getReadableTimeBetween(first, last, reverse=False):
+    timeBetween = int(last-first)
+    now = datetime.datetime.now()
+    year = now.year
+    month = now.month
+
+    timeBetween, years, year = get_years(timeBetween, year, reverse)
+    timeBetween, months = get_months(timeBetween, year, month, reverse)
+    
+    weeks   = int(timeBetween/604800)
+    days    = int((timeBetween-(weeks*604800))/86400)
+    hours   = int((timeBetween-(days*86400 + weeks*604800))/3600)
+    minutes = int((timeBetween-(hours*3600 + days*86400 + weeks*604800))/60)
+    seconds = int(timeBetween-(minutes*60 + hours*3600 + days*86400 + weeks*604800))
+    msg = ""
+    
+    if years > 0:
+        msg += "1 year, " if years == 1 else "{:,} years, ".format(years)
+    if months > 0:
+        msg += "1 month, " if months == 1 else "{:,} months, ".format(months)
+    if weeks > 0:
+        msg += "1 week, " if weeks == 1 else "{:,} weeks, ".format(weeks)
+    if days > 0:
+        msg += "1 day, " if days == 1 else "{:,} days, ".format(days)
+    if hours > 0:
+        msg += "1 hour, " if hours == 1 else "{:,} hours, ".format(hours)
+    if minutes > 0:
+        msg += "1 minute, " if minutes == 1 else "{:,} minutes, ".format(minutes)
+    if seconds > 0:
+        msg += "1 second, " if seconds == 1 else "{:,} seconds, ".format(seconds)
+
+    if msg == "":
+        return "0 seconds"
+    else:
+        return msg[:-2]	
 
 def get_seconds(timeString: str) -> Optional[int]:
     time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
@@ -46,25 +128,24 @@ async def download_video(video_code : str):
     time = get_seconds(timeString=timeString)
     if not time: 
         return {"status":"error", "message":"Unknown error."}
-    elif time > 15*60:
-        return {"status":"error", "message":"Video duration more than 15 minutes."}
+    # elif time > 15*60:
+    #     return {"status":"error", "message":"Video duration more than 15 minutes."}
     else:
         url = f'https://www.youtube.com/watch?v={video_code}'
         yt_obj = YouTube(url)
         best = sorted(yt_obj.streams.filter(type="video"), key=lambda stream: int(stream.resolution.replace("p", "")), reverse=True)[0]
         lowest = sorted(yt_obj.streams.filter(type="video"), key=lambda stream: int(stream.resolution.replace("p", "")), reverse=False)[0]
         audio = sorted(yt_obj.streams.filter(type="audio"), key=lambda stream: int(stream.bitrate))[0]
-        data["thumbnail"] = results["items"][0]["snippet"]["thumbnails"]["standard"]["url"]
+        data["thumbnail"] = results["items"][0]["snippet"]["thumbnails"]["maxres"]["url"]
         data["title"] = results["items"][0]["snippet"]["title"]
         data["duration"] = time
-        data["options"] = {"highest": {"res":best.resolution,"fps":best.fps},
-                           "lowest": {"res":lowest.resolution,"fps":lowest.fps},
-                           "audio": {"bitrate": audio.bitrate}}
+        data["highest"] = best.resolution + str(best.fps)
+        data["lowest"] = lowest.resolution + str(lowest.fps)
         return data
  
 @app.route('/')
 async def index():
-    return await render_template('index.html', download_video=download_video)
+    return await render_template('index.html', getReadableTimeBetween=getReadableTimeBetween)
 
 @app.route('/convert/<code>')
 async def convert(code):
